500d5d97f916025f7a6acd020eec8289ce5057b4
Kateryna <k.borysenko.kyiv@gmail.com>
2024-03-11 19:10:18 +0200

    Normalize CSS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
+import React from 'react'
+import '../../assets/styles/global.module.css'
 import s from './App.module.css'
 
 const App = () => {
-  return (
-    <div className={s.container} >You're welcome to Phonebook !</div>
-  )
+  return <div className={s.container}>You're welcome to Phonebook !</div>
 }
 
 export default App
