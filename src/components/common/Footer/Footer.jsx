3896810375932db1af222a33e4d85d4dc6553bcb
Kateryna <k.borysenko.kyiv@gmail.com>
2024-03-27 14:07:33 +0200

    fix:open link in new tab in footer

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <footer className={s.footer}>
       © {new Date().getFullYear()}&nbsp;
-      <a href="https://github.com/Kateryna-Borysenko" className="Link">
+      <a
+        href="https://github.com/Kateryna-Borysenko"
+        className="Link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         &nbsp; <span className={s.title}>PhoneBook</span>
       </a>
       &nbsp; All rights reserved.
